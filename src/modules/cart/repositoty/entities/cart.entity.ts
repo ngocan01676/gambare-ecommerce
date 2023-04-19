962679a924a953d2d755cac94fe01a02370250e1
Vu Ngoc An <AnVN@vietis.vn>
2023-04-19 16:52:27 +0700

    Cart entity model

--- src/modules/cart/repositoty/entities/cart.entity.ts
+++ src/modules/cart/repositoty/entities/cart.entity.ts
@@ -32,6 +32,93 @@ export class CartEntity extends DatabaseMongoUUIDEntityAbstract {
   })
   addressId: string;
 
+  @Prop({
+    required: false,
+    type: String
+  })
+  productId: string;
+
+  @Prop({
+    required: false,
+    type: String
+  })
+  variation: string;
+
+  @Prop({
+    required: false,
+    type: Number,
+    default: 0
+  })
+  price: number;
+
+  @Prop({
+    required: false,
+    type: Number,
+    default: 0
+  })
+  tax: number;
+
+  @Prop({
+    required: true,
+    type: Number,
+    default: 0
+  })
+  shippingCode: number;
+    
+  @Prop({
+    required: true,
+    type: String,
+    default: ''
+  })
+  shippingType: string;
+
+  @Prop({
+    required: false,
+    type: Number,
+  })
+  pickupPoint: number;
+
+  @Prop({
+    required: true,
+    type: Number,
+    default: 0
+  })
+  discount: number;
+
+  @Prop({
+    required: false,
+    type: String,
+    default: 0
+  })
+  productReferralCode: string;
+
+  @Prop({
+    required: false,
+    type: String
+  })
+  couponCode: string;
+
+  @Prop({
+    required: true,
+    type: Number,
+    default: 0
+  })
+  couponApplied: number;
+
+  @Prop({
+    required: true,
+    type: Number,
+    default: 0
+  })
+  quantity: number;
+
+  @Prop({
+    required: true,
+    type: Number,
+    default: 0
+  })
+  selectedItem: number;
+
 }
 
 export const CartSchema = SchemaFactory.createForClass(CartEntity);
