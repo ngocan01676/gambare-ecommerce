12333de437e906983e0b4f11a7e81ee43b55a72d
Vu Ngoc An <AnVN@vietis.vn>
2023-04-25 17:27:54 +0700

    Update

---
+++ src/modules/uploads/repository/entities/upload.entity.ts
@@ -0,0 +1,59 @@
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { CallbackWithoutResultAndOptionalError } from 'mongoose';
+import { DatabaseMongoUUIDEntityAbstract } from 'src/common/database/abstracts/mongo/entities/database.mongo.uuid.entity.abstract';
+import { DatabaseEntity } from 'src/common/database/decorators/database.decorator';
+
+const UploadsDatabaseName = 'uploads';
+@DatabaseEntity({ collection: UploadsDatabaseName })
+export class UploadEntity extends DatabaseMongoUUIDEntityAbstract {
+    @Prop({
+        required: false,
+        type: String,
+    })
+    fileOriginalName: string;
+
+    @Prop({
+        required: false,
+        type: String,
+    })
+    fileName: string;
+
+    @Prop({
+        required: false,
+        type: String,
+    })
+    userId: string;
+
+    @Prop({
+        required: false,
+        type: Number,
+    })
+    fileSize: number;
+
+    @Prop({
+        required: false,
+        type: String,
+    })
+    extension: string;
+
+    @Prop({
+        required: false,
+        type: String,
+    })
+    type: string;
+
+    @Prop({
+        required: false,
+        type: String,
+    })
+    externalLink: string;
+}
+
+export const UploadSchema = SchemaFactory.createForClass(UploadEntity);
+
+UploadSchema.pre(
+    'save',
+    function (next: CallbackWithoutResultAndOptionalError) {
+        next();
+    }
+);
